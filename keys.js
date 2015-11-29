4c6bffe0b0bc9e783ad7255ce5e4fe526a10443a
Dhruv Mehrotra <dhruv.mehrotra3@gmail.com>
2015-11-29 15:05:03 -0500

    first commit

---
+++ keys.js
@@ -0,0 +1,8 @@
+module.exports = function() {
+  return {
+    // consumer_key:insert keys here
+    // consumer_secret:insert keys here
+    // access_token_key:insert keys here
+    // access_token_secret:insert keys here
+  };
+}
