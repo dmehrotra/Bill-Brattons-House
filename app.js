4d4af77ce09668d9c64f84f7eec6414280550060
Dhruv Mehrotra <dhruv.mehrotra3@gmail.com>
2015-11-29 19:56:02 -0500

    restructure

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var multer          =       require('multer');
 var app             =       express();
 var upload          =       multer({ dest: './uploads/'});
 var port = process.env.PORT || 8080;
+var Tweeter         =       require('twitter');
+var Stream          =       require('node-tweet-stream')
+var Keys            =       require('./keys.js')();
 
 app.use(express.static('uploads'));
 
@@ -35,4 +38,34 @@ app.post('/api/photo',function(req,res){
 
 app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);
+
+    var client = new Tweeter({
+      consumer_key: Keys.consumer_key,
+      consumer_secret: Keys.consumer_secret,
+      access_token_key: Keys.access_token_key,
+      access_token_secret: Keys.access_token_secret
+    });
+
+    var stream = new Stream({
+        consumer_key: Keys.consumer_key,
+        consumer_secret: Keys.consumer_secret,
+        token: Keys.access_token_key,
+        token_secret: Keys.access_token_secret
+      })
+     
+    stream.on('tweet', function (tweet) {
+      console.log('tweet received', tweet)
+      client.post('statuses/update', {status: "@"+tweet.user.screen_name + " Sup"}, function(error, tweet, response){})
+    })
+
+    stream.on('error', function (err) {
+      console.log('Oh no')
+    })
+
+    console.log('running');
+    stream.track('brttncm');
+    stream.track('@dmehro');
+
+// Todo separate app and stream
+
 });
--- stream.js
+++ stream.js
@@ -1,35 +1,35 @@
-var Tweeter         =       require('twitter');
-var Stream          =       require('node-tweet-stream')
-var Keys            =       require('./keys.js')();
+// var Tweeter         =       require('twitter');
+// var Stream          =       require('node-tweet-stream')
+// var Keys            =       require('./keys.js')();
 
-// 
+// // 
 
-var client = new Tweeter({
-  consumer_key: Keys.consumer_key,
-  consumer_secret: Keys.consumer_secret,
-  access_token_key: Keys.access_token_key,
-  access_token_secret: Keys.access_token_secret
-});
+// var client = new Tweeter({
+//   consumer_key: Keys.consumer_key,
+//   consumer_secret: Keys.consumer_secret,
+//   access_token_key: Keys.access_token_key,
+//   access_token_secret: Keys.access_token_secret
+// });
 
-var stream = new Stream({
-    consumer_key: Keys.consumer_key,
-    consumer_secret: Keys.consumer_secret,
-    token: Keys.access_token_key,
-    token_secret: Keys.access_token_secret
-  })
+// var stream = new Stream({
+//     consumer_key: Keys.consumer_key,
+//     consumer_secret: Keys.consumer_secret,
+//     token: Keys.access_token_key,
+//     token_secret: Keys.access_token_secret
+//   })
  
-stream.on('tweet', function (tweet) {
-  console.log('tweet received', tweet)
-  client.post('statuses/update', {status: "@"+tweet.user.screen_name + " Sup"}, function(error, tweet, response){})
-})
+// stream.on('tweet', function (tweet) {
+//   console.log('tweet received', tweet)
+//   client.post('statuses/update', {status: "@"+tweet.user.screen_name + " Sup"}, function(error, tweet, response){})
+// })
 
-stream.on('error', function (err) {
-  console.log('Oh no')
-})
+// stream.on('error', function (err) {
+//   console.log('Oh no')
+// })
 
-console.log('running');
-stream.track('brttncm');
-stream.track('@dmehro');
+// console.log('running');
+// stream.track('brttncm');
+// stream.track('@dmehro');
 
-// Todo separate app and stream
+// // Todo separate app and stream
 
