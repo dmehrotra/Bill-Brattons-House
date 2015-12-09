2c40ebe5aa5d06339f1214f9a17b36f87db5b79f
Dhruv Mehrotra <dhruv.mehrotra3@gmail.com>
2015-12-09 15:42:51 -0500

    fix

--- keys.js
+++ keys.js
@@ -2,8 +2,7 @@ module.exports = function() {
   return {
     consumer_key: '0oEJ7XnarxGkbwnY1d5dbHMHF',
     consumer_secret: 'lcZRJSBk4ShLmJikMKZa1AQljMssozps5ksTqdRohGTkLbxMBN',
-    access_token_key: '4376912182-XcV3x6oK3MZiBfBwp2jKPqPGDOTMyhCsuCORwOt
-',
+    access_token_key: '4376912182-XcV3x6oK3MZiBfBwp2jKPqPGDOTMyhCsuCORwOt',
     access_token_secret: 'ZQkual6AcT9M3OLQ0MlFcDH9Lj4hq8wvrVzMgFqssLafK'
   };
 }
