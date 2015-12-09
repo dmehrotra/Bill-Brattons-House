4a717c48b60177d60746511141b52fbecaaf41d8
Dhruv Mehrotra <dhruv.mehrotra3@gmail.com>
2015-12-09 18:11:15 -0500

    ss

--- keys.js
+++ keys.js
@@ -2,7 +2,7 @@ module.exports = function() {
   return {
     consumer_key: 'r5jsCecxowCbCJXNPcuM8wfwL',
     consumer_secret: 'Q5NZfUH7tc2a7cMi6ezOFSLeUW9dkeclgNxqvpDL2uXexDUlcb',
-    access_token_key: '	4376912182-OO1NHQnGRxk9QEtvPnPGmO42WEnbKDSFKlE4XUn',
-    access_token_secret: '	HiJoSLNUGkFy4rnJL5Fip0iE6sPcxZ6XjMaCrtCxc9CP5'
+    access_token_key: '4376912182-OO1NHQnGRxk9QEtvPnPGmO42WEnbKDSFKlE4XUn',
+    access_token_secret: 'HiJoSLNUGkFy4rnJL5Fip0iE6sPcxZ6XjMaCrtCxc9CP5'
   };
 }
